abf9c4878a85929f13f8cb974d516bc901fee07a
Andrew <escapeanaemia@gmail.com>
2020-08-28 14:17:42 +0900

    init project

---
+++ MusicCrawler/MusicCrawler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicCrawler
+//
+//  Created by Andrew Han on 2020/08/28.
+//  Copyright © 2020 escapeanaemia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MusicCrawler/Pods/Target Support Files/Pods-MusicCrawlerTests/Pods-MusicCrawlerTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MusicCrawlerTests : NSObject
+@end
+@implementation PodsDummy_Pods_MusicCrawlerTests
+@end
